2aaa80a3ce8ae7eadb1369f9130992c72f60e5ef
Pedro Oliveira <pedricky63@gmail.com>
2025-05-06 16:54:28 +0000

    1.2

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,42 +1,42 @@
 #include <stdio.h>
 
-void iniciograde(char grade[10][10]){
-
-    for (int i = 0; i < 10; i++)
-    {
-        for (int j = 0; j < 10; j++){
-            grade[i][j] = '0';
-        }
-        
-    }
-    
-}
-
-void ExibicaoGrade(char grade[10][10]){
-    printf("   ");
-    for (char letra = 'A'; letra < 'A' + 10; letra++){
-        printf(" %c", letra);
-    }
-    printf("\n");
-    
-    for (int i = 0; i < 10; i++){
-        printf("%2d ", i + 1);
-        for (int j = 0; j < 10; j++){
-            printf("%c ", grade[i][j]);
-        }
-        printf("\n");        
-    }    
-}
 
 int main(){
+    char area[10][10];
+    char barco[10][10];
 
-    printf(" ### BATALHA NAVAL ###\n");
+   for (int i = 0; i < 10; i++){ 
+       for (int j = 0; j < 10; j++){
+        area[i][j] = '0';
+       }       
+   }   
+    
+    for (int i = 1; i < 4; i++){
+        area[8][i] = '3';
+    }
 
-    char grade[10][10];
-    iniciograde(grade);
-    ExibicaoGrade(grade);
+    for (int i = 1; i < 4; i++){
+        area[i][3] = '3';
+    }
 
+        printf("\n");
+    
 
+    printf("  ");
+     for (char letra = 'A'; letra <= 'J'; letra++){
+        printf(" %1c", letra);
+     }
+     printf("\n");
 
-    return 0;
+     for (int i = 0; i < 10; i++){
+        printf("%3d ", i+1);
+        for (int j = 0; j < 10; j++){
+            printf("%c ", area[i][j]);
+        }
+        printf("\n");
+     }
+     
+     
+
+        return 0;
 }
